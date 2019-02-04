39a7db0c3fab44fb4487a84f9c4278c677b7ef37
ROBIN Eliott <liopty@gmail.com>
2019-02-04 15:30:00 +0100

    csv à la place des json

---
+++ app/routes/api/installations.js
@@ -0,0 +1,16 @@
+/* Load Modules */
+const express = require('express');
+const router = express.Router();
+
+/* Load controller */
+//const CarController = require('../../controller/carController');
+//const carController = new CarController();
+
+/**
+ * Car Entity routes
+ */
+//router.get('/count', function (req, res) {
+//    carController.countAll(res);
+//});
+
+//module.exports = router;
--- app/routes/router.js
+++ app/routes/router.js
@@ -5,7 +5,8 @@ const express = require('express');
 const router = express.Router();
 
 /* API routes */
-//router.use('/car', require('./api/carRoutes'));
-//router.use('/driver', require('./api/driverRoutes'));
+router.use('/activitesFichesEquipement', require('.api/activitesFichesEquipements'));
+router.use('/equipements', require('./api/equipements'));
+router.use('/installations', require('./api/installations'));
 
 module.exports = router;
