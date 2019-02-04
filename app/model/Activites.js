a16ef233ccec6a1db6969d24ebb20d4fe8c2cb4f
ROBIN Eliott <liopty@gmail.com>
2019-02-04 16:21:16 +0100

    fin setup fichiers

--- app/model/Activites.js
+++ app/model/Activites.js
@@ -1,6 +1,15 @@
 /**
- * Classe activites
+ * Car Entity (ES6 Class)
  */
-class activites {
 
+class Activites {
+  /*  constructor(id, maker, model, year, driver) {
+        this.id = id;
+        this.maker = maker;
+        this.model = model;
+        this.year = year;
+        this.driver = driver;
+    }*/
 }
+
+module.exports = Activites;
---
+++ app/model/Equipement.js
@@ -0,0 +1,15 @@
+/**
+ * Car Entity (ES6 Class)
+ */
+
+class Equipements {
+  /*  constructor(id, maker, model, year, driver) {
+        this.id = id;
+        this.maker = maker;
+        this.model = model;
+        this.year = year;
+        this.driver = driver;
+    }*/
+}
+
+module.exports = Equipements;
